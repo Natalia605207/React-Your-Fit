56a0a6a3fdcfce3fc573141b967097a2b36b8202
Natalia605207 <natalyaobukhova87@gmail.com>
2024-05-31 19:20:39 -0400

    created Video component for video

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,13 +1,11 @@
-import "../App.css";
-import video from "../assets/cover.mp4";
 import { HashLink as Link } from 'react-router-hash-link';
+import AutoPlaySilentVideo from './Video';
+import "../App.css";
 
 export const FrontPage = () => {
     return(
         <section className="top-cover column">
-            <video oop={true} muted={true} autoplay={true} playsinline={true} controls>
-                <source src={video} type="video/mp4"/>
-            </video>
+            <AutoPlaySilentVideo />
             <h2>Welcome to <span className="outlined-words">&nbsp;YOUR FIT&nbsp;</span></h2>
             <h1>Stretching <span className="plus">+</span> fitness <span className="plus">+</span> yoga studio</h1>
             <h3>Dive into the world of movement, energy and unique training classes</h3>
---
+++ src/components/Video.js
@@ -0,0 +1,20 @@
+import React, {useRef, useEffect} from "react";
+import "../App.css";
+import cover from "./cover.mp4";
+
+export default function AutoPlaySilentVideo() {
+    const videoRef = useRef(undefined);
+    useEffect(() => {
+        videoRef.current.defaultMuted = true;
+    })
+    return (
+        <video
+            ref={videoRef}
+            loop
+            autoPlay
+            muted
+            playsInline>
+            <source src={cover} type="video/mp4"/>
+        </video>
+    );
+}
