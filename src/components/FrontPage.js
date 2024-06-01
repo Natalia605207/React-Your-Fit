bd6a61607b35fe4d85c6393bf21e1628b0c359c5
Natalia605207 <natalyaobukhova87@gmail.com>
2024-05-31 20:02:27 -0400

    changed video component

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,11 +1,13 @@
 import { HashLink as Link } from 'react-router-hash-link';
 import "../App.css";
-import Video from './Video';
+import cover from "./cover.mp4";
 
 export const FrontPage = () => {
     return(
         <section className="top-cover column">
-            <Video />
+            <video autoPlay muted loop playsInline>
+                <source src={cover} type="video/mp4"></source>
+            </video>
             <h2>Welcome to <span className="outlined-words">&nbsp;YOUR FIT&nbsp;</span></h2>
             <h1>Stretching <span className="plus">+</span> fitness <span className="plus">+</span> yoga studio</h1>
             <h3>Dive into the world of movement, energy and unique training classes</h3>
