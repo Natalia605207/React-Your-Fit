5ef83429af7c0c534be14e97750ca6e4efe54237
Natalia605207 <natalyaobukhova87@gmail.com>
2024-05-31 20:33:25 -0400

    changed cover photo

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,13 +1,11 @@
 import { HashLink as Link } from 'react-router-hash-link';
 import "../App.css";
-import cover from "./cover.mp4";
+import mainPhoto from "../assets/main-photo.jpg";
 
 export const FrontPage = () => {
     return(
         <section className="top-cover column">
-            <video autoPlay muted loop playsInline>
-                <source src={cover} type="video/mp4"></source>
-            </video>
+            <img src={mainPhoto} alt="cover" className="main-photo" />
             <h2>Welcome to <span className="outlined-words">&nbsp;YOUR FIT&nbsp;</span></h2>
             <h1>Stretching <span className="plus">+</span> fitness <span className="plus">+</span> yoga studio</h1>
             <h3>Dive into the world of movement, energy and unique training classes</h3>
